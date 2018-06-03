6c65459a6625ca99ccfdc909cdb92d59e38e35ea
Travis CI User <travis@example.org>
2018-06-03 09:10:04 +0000

    Update documentation

---
+++ implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wrapped2d"] = [{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.65/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"wrapped2d/common/math/struct.Vec2.html\" title=\"struct wrapped2d::common::math::Vec2\">Vec2</a>",synthetic:false,types:["wrapped2d::common::math::Vec2"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wrapped2d"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.65/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"wrapped2d/common/math/struct.Vec2.html\" title=\"struct wrapped2d::common::math::Vec2\">Vec2</a>",synthetic:false,types:["wrapped2d::common::math::Vec2"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
